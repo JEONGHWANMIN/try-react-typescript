f2dea4c1a8360e4ec1272b5d81342680f5a3f65d
JeongHwanMin <65848374+JEONGHWANMIN@users.noreply.github.com>
2022-03-23 23:31:57 +0900

    useReducer Type

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,20 @@
-import './App.css';
-import Button from './components/Button';
-import Container from './components/Container';
-import Greet from './components/Greet';
-import Heading from './components/Heading';
-import Input from './components/Input';
-import Oscar from './components/Oscar';
-import Person from './components/Person';
-import PersonList from './components/PersonList';
-import Status from './components/Status';
+import "./App.css";
+import Button from "./components/Button";
+import Container from "./components/Container";
+import Greet from "./components/Greet";
+import Heading from "./components/Heading";
+import Input from "./components/Input";
+import Oscar from "./components/Oscar";
+import Person from "./components/Person";
+import PersonList from "./components/PersonList";
+import Counter from "./components/state/Counter";
+import Status from "./components/Status";
 function App() {
-  const personName = {
-    first: 'HwanMin',
-    last: 'Jeong',
-  };
-  const personList = [
-    {
-      first: 'HwanMin',
-      last: 'Jeong',
-    },
-    {
-      first: 'HwanMin1',
-      last: 'Jeong1',
-    },
-    {
-      first: 'HwanMin2',
-      last: 'Jeong2',
-    },
-  ];
-  return (
-    <div className='App'>
-      <Button
-        handleClick={(event, id) => {
-          console.log('Button Clicked', event, id);
-        }}
-      />
-      <Input />
-      <Container
-        styles={{ border: 'solid 1px red', padding: '1rem', display: 'flex' }}
-      />
-    </div>
-  );
+	return (
+		<div className="App">
+			<Counter />
+		</div>
+	);
 }
 
 export default App;
