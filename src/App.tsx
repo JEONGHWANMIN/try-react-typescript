5f96ac2adb721580279107c9819d95a333f3bf8f
JeongHwanMin <65848374+JEONGHWANMIN@users.noreply.github.com>
2022-04-04 23:11:21 +0900

    type : never

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import './App.css';
-import { Counter } from './components/class/Counter';
-import List from './components/generics/List';
+import "./App.css";
+import { Counter } from "./components/class/Counter";
+import List from "./components/generics/List";
+import RandomNumber from "./components/restriction/RandomNumber";
 function App() {
-  return (
-    <div className='App'>
-      <List
-        items={['Batman', 'Superman', 'Wonder Woman']}
-        onClick={(item) => console.log(item)}
-      />
-    </div>
-  );
+	return (
+		<div className="App">
+			<RandomNumber value={10} isPositive />
+		</div>
+	);
 }
 
 export default App;
