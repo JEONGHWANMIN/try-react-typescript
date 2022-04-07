bfae52fc6a623edf932f5f064097a7baf3ca47b5
JEONGHWANMIN <mimi0j0j@naver.com>
2022-04-07 22:53:42 +0900

    React.ComponentProps

---
+++ src/components/html/CustomComponent.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Greet from '../Greet';
+
+function CustomComponent(props: React.ComponentProps<typeof Greet>) {
+  return <div>{props.isLoggedIn}</div>;
+}
+
+export default CustomComponent;
