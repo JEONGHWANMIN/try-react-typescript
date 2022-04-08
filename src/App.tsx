9875e387129fcf573ea3e2c1967ea8982986ba62
JeongHwanMin <65848374+JEONGHWANMIN@users.noreply.github.com>
2022-04-09 01:34:22 +0900

    polymorphic

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import './App.css';
-import { Counter } from './components/class/Counter';
-import List from './components/generics/List';
-import RandomNumber from './components/restriction/RandomNumber';
-import Toast from './components/templateliterles/Toast';
+import "./App.css";
+import Text from "./components/polymorphic/Text";
 function App() {
-  return (
-    <div className='App'>
-      <Toast position={'right-bottom'} />
-    </div>
-  );
+	return (
+		<div className="App">
+			<Text size="lg" as={"h1"}>
+				Heading
+			</Text>
+			<Text size="md" as="p">
+				Phragraph
+			</Text>
+			<Text size="sm" color="secondary">
+				Label
+			</Text>
+		</div>
+	);
 }
 
 export default App;
--- src/components/polymorphic/Text.tsx
+++ src/components/polymorphic/Text.tsx
@@ -1,13 +1,27 @@
 import React from "react";
 
-type TextProps = {
-	size?: "sm" | "sm" | "lg";
+type TextOwnProps<E extends React.ElementType> = {
+	size?: "sm" | "md" | "lg";
 	color?: "primary" | "secondary";
 	children: React.ReactNode;
+	as?: E;
 };
 
-function Text({ size, color, children }: TextProps) {
-	return <div>Text</div>;
+type TextProps<E extends React.ElementType> = TextOwnProps<E> &
+	Omit<React.ComponentProps<E>, keyof TextOwnProps<E>>;
+
+function Text<E extends React.ElementType = "div">({
+	as,
+	size,
+	color,
+	children,
+}: TextProps<E>) {
+	const Component = as || "div";
+	return (
+		<Component className={`class-with-${size}-${color}`}>
+			{children}
+		</Component>
+	);
 }
 
 export default Text;
